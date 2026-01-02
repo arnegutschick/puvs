d7382608374279745ab5ac075f42902b186ba1a7
Arne Gutschick <argu8599@th-wildau.de>
2026-01-02 12:01:26 +0100

    another parameter consistency fix

--- ChatServer/Models/TopChatter.cs
+++ ChatServer/Models/TopChatter.cs
@@ -4,6 +4,6 @@ namespace ChatServer;
 /// Represents a user and their total number of messages,
 /// used for generating leaderboards or top chatters statistics.
 /// </summary>
-/// <param name="User">The username of the chat participant.</param>
+/// <param name="UserName">The username of the chat participant.</param>
 /// <param name="MessageCount">The total number of messages sent by the user.</param>
-public record TopChatter(string User, int MessageCount);
+public record TopChatter(string UserName, int MessageCount);
